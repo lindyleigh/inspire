b8a9222ebaf61e43e80e157690b1d7c7ee4c8126
Lindy Crain <lindylcrain@gmail.com>
2016-09-07 23:13:40 -0600

    fixed tiles

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,7 +1,4 @@
 (function(){
-// // 	// new up the TodoService that has already been configured for your use
-// // 	// There are two methods getTodos returns and array
-// // 	// saveTodos accepts an array and stores it to your local storage
 	
 	var todoService = new TodoService();
 
@@ -64,55 +61,6 @@
 	
 }())
 
-
-//  Another way to do a to-do form
-
-	//var todoService = new ToDoService();
-	// var todos= todoService.getTodos()
-	
-
-// 	var todos = []
-
-//    $('.todo-form').on('submit', function(e) {
-//  		e.preventDefault()
-//  		var form = this;
-//  		var input = form.toDoInput.value; 
-		
-
-//  		todos.push(input);
-//  		todoService.saveTodos(todos);
-//  		update();
-
-		// $('.todo-list').on('click', '.item', function(event) {
-		// 	var task = $(event.target)
-			// removeTask(task);
-			// update();
-
-		// 	})
-
-		// function removeTask(name) [
-		// 	for(var i=0; i <todos.length; i++) {
-		// 		var currentTask = todos[i];
-		// 		if(currentTask == name) {
-		// 			return todos.splice[i, 1];
-		
-		// 		}
-		// 	}
-		// 
-		// todoService.saveTodos(todos);
-		// update()
-
-//  	function update() {
-		// $('.todo-list').empty();
-// 		 for (var i= 0; i < todos.length; i++) {
-// 			 var currentTask = todos [i];
-
-// 			 $('.todo-list').append(`
-// 			 ----------
-			 
-// 			 `)
-// 		 }
-// 	 }
 
 
 
